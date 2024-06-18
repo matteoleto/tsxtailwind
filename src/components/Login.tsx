bd6c810df6c0868fac310b8e0bfd5771f7e2c1f0
matteoleto <matteo.leto20@gmail.com>
2024-06-18 15:45:26 +0200

    bottoni

---
+++ src/components/Login.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+interface LoginProps {
+  onLogin: (username: string) => void;
+}
+
+const Login: React.FC<LoginProps> = ({ onLogin }) => {
+  const [username, setUsername] = useState<string>('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (username.trim()) {
+      onLogin(username);
+    }
+  };
+  
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+    <form onSubmit={handleSubmit} className="bg-white p-6 rounded shadow-md">
+      <div className="mb-4">
+        <img
+          src="/src/img/zextras-logo-vector 1.png"  // Sostituisci con il percorso dell'immagine desiderata
+          alt="Logo"  // Aggiungi un testo alternativo per accessibilità
+          className="mx-auto w-auto h-20 mb-4"
+        />
+      </div>
+      <h2 className="text-2xl font-bold mb-4 text-blue-700">Username</h2>
+      <input
+        type="text"
+        placeholder="Inserisci il tuo username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        className="w-full p-2 border border-gray-300 rounded mb-4"
+      />
+      <h2 className="text-2xl font-bold mb-4 text-blue-700">Password</h2>
+      <input
+        type="text"
+        placeholder="Inserisci la tua password"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        className="w-full p-2 border border-gray-300 rounded mb-4"
+      />
+      <button type="submit" className="w-full p-2 bg-blue-500 text-white rounded">
+        Login
+      </button>
+    </form>
+  </div>
+  
+  );
+};
+
+export default Login;
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,33 +1,52 @@
 import React, { useState } from 'react';
 
-export let colorePrincipale:number
+export let colorePrincipale: number;
 
 const Sidebar: React.FC = () => {
-  const [colore, setColore] = useState<string>(cambiaColore(3))
+  const [colore, setColore] = useState<string>(cambiaColore(3));
+
   return (
-    <div className={"bg-menu-bg w-72 h-screen p-4 bg-"+colore+"-600"}>
-      <h2 className="text-4xl font-bold mb-4 text-white pt-4"></h2>
-      <button className='bg-blue-600'  onClick={()=>setColore(cambiaColore(0))}></button>
-      <button className='bg-red-600' onClick={()=>setColore(cambiaColore(1))}></button>
-      <button className='bg-green-600' onClick={()=>setColore(cambiaColore(2))}></button>
-      <button className='bg-purple-600' onClick={()=>setColore(cambiaColore(3))}></button>
-      <button className='bg-yellow-600' onClick={()=>setColore(cambiaColore(4))}></button>
+    <div className={`bg-${colore}-600 h-screen w-72 p-4`}>
+      <h2 className="mb-4 pt-4 text-4xl font-bold text-white"></h2>
+      <div className="bg-gray-800 p-4 rounded-lg">
+        <div className="flex flex-col gap-2">
+          <button
+            className="h-10 w-10 rounded-full bg-blue-600 shadow-xl hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 transform transition-transform hover:-translate-y-2 active:translate-y-0 active:shadow-none"
+            onClick={() => setColore(cambiaColore(0))}
+          ></button>
+          <button
+            className="h-10 w-10 rounded-full bg-red-600 shadow-xl hover:bg-red-700 focus:ring-4 focus:ring-red-300 transform transition-transform hover:-translate-y-2 active:translate-y-0 active:shadow-none"
+            onClick={() => setColore(cambiaColore(1))}
+          ></button>
+          <button
+            className="h-10 w-10 rounded-full bg-green-600 shadow-xl hover:bg-green-700 focus:ring-4 focus:ring-green-300 transform transition-transform hover:-translate-y-2 active:translate-y-0 active:shadow-none"
+            onClick={() => setColore(cambiaColore(2))}
+          ></button>
+          <button
+            className="h-10 w-10 rounded-full bg-purple-600 shadow-xl hover:bg-purple-700 focus:ring-4 focus:ring-purple-300 transform transition-transform hover:-translate-y-2 active:translate-y-0 active:shadow-none"
+            onClick={() => setColore(cambiaColore(3))}
+          ></button>
+          <button
+            className="h-10 w-10 rounded-full bg-yellow-600 shadow-xl hover:bg-yellow-700 focus:ring-4 focus:ring-yellow-300 transform transition-transform hover:-translate-y-2 active:translate-y-0 active:shadow-none"
+            onClick={() => setColore(cambiaColore(4))}
+          ></button>
+        </div>
+      </div>
     </div>
   );
-  function cambiaColore(n:number):string{
-    let coloreSelezionato:string=""
-    switch(n)
-    {
-      case 1: coloreSelezionato="red"; break;
-      case 2: coloreSelezionato="green"; break;
-      case 3: coloreSelezionato="purple"; break;
-      case 4: coloreSelezionato="yellow"; break;
-      default: coloreSelezionato="blue"; break;
+
+  function cambiaColore(n: number): string {
+    let coloreSelezionato = "";
+    switch (n) {
+      case 1: coloreSelezionato = "red"; break;
+      case 2: coloreSelezionato = "green"; break;
+      case 3: coloreSelezionato = "purple"; break;
+      case 4: coloreSelezionato = "yellow"; break;
+      default: coloreSelezionato = "blue"; break;
     }
-    colorePrincipale= n
-    return coloreSelezionato
+    colorePrincipale = n;
+    return coloreSelezionato;
   }
 };
-
 
 export default Sidebar;
