03e10ea4b5d2fe2e80751aa1593003b46af61c7f
Davide <davide.delbianco06@gmail.com>
2024-06-24 17:46:47 +0200

    added sidebartest that works

--- src/test/Sidebar.test.tsx
+++ src/test/Sidebar.test.tsx
@@ -1,22 +1,27 @@
 import React from "react";
 
-import { render, screen } from "@testing-library/react";
-import { describe, it, expect } from "vitest";
+import { render, screen, act } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+import { describe, expect, it, vi } from "vitest";
 
 import Sidebar from "../components/Sidebar";
 import { ThemeContext } from "../components/ThemeContext";
 
 describe("SideBar", () => {
-  const context = { color: "blue", setColor: (): void => {} };
-  it("should check if the button has been clicked", () => {
-    const btnProva = <button id="blue button"></button>;
-    render(
-      <ThemeContext.Provider value={context}>
-        <Sidebar />
-      </ThemeContext.Provider>,
-    );
-    expect(screen.getByTestId(`${context.color} button`)).toMatchObject(
-      btnProva,
-    );
-  });
+  const setColor = vi.fn();
+  const context = { color: "blue", setColor };
+  it.each(["blue", "red", "green", "purple", "yellow"])(
+    "should check if the button %s has been clicked",
+    async (color) => {
+      const user = userEvent.setup();
+      render(
+        <ThemeContext.Provider value={context}>
+          <Sidebar />
+        </ThemeContext.Provider>,
+      );
+      const btnClicked = screen.getByTestId(`${color} button`);
+      await act(async () => user.click(btnClicked));
+      expect(setColor).toHaveBeenCalledWith(color);
+    },
+  );
 });
