0e5793c3c8da6bffd5b4a8b132b537b8a8ed7c7e
matteoleto <matteo.leto20@gmail.com>
2024-06-24 14:26:50 +0200

    c

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -31,39 +31,7 @@ const Sidebar: React.FC = () => {
     }
     return coloreSelezionato;
   }
-
   return (
-<<<<<<< HEAD
-    <div className={`bg-${color}-600 h-screen w-72 p-4 flex items-center justify-center`}>
-      <div className="bg-gray-100 p-4 rounded-lg shadow-lg">
-        <div className="flex flex-col gap-2">
-          <button
-            id="blue button"
-            className="h-10 w-10 rounded-full bg-blue-600 shadow-xl hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 transform transition-transform hover:-translate-y-2 active:translate-y-0 active:shadow-none text-blue-600"
-            onClick={() => setColor(cambiaColore(0))}
-          ></button>
-          <button
-            id="red button"
-            className="h-10 w-10 rounded-full bg-red-600 shadow-xl hover:bg-red-700 focus:ring-4 focus:ring-red-300 transform transition-transform hover:-translate-y-2 active:translate-y-0 active:shadow-none text-blue-600"
-            onClick={() => setColor(cambiaColore(1))}
-          ></button>
-          <button
-            id="green button"
-            className="h-10 w-10 rounded-full bg-green-600 shadow-xl hover:bg-green-700 focus:ring-4 focus:ring-green-300 transform transition-transform hover:-translate-y-2 active:translate-y-0 active:shadow-none text-blue-600"
-            onClick={() => setColor(cambiaColore(2))}
-          ></button>
-          <button
-            id="purple button"
-            className="h-10 w-10 rounded-full bg-purple-600 shadow-xl hover:bg-purple-700 focus:ring-4 focus:ring-purple-300 transform transition-transform hover:-translate-y-2 active:translate-y-0 active:shadow-none text-blue-600"
-            onClick={() => setColor(cambiaColore(3))}
-          ></button>
-          <button
-            id="yellow button"
-            className="h-10 w-10 rounded-full bg-yellow-600 shadow-xl hover:bg-yellow-700 focus:ring-4 focus:ring-yellow-300 transform transition-transform hover:-translate-y-2 active:translate-y-0 active:shadow-none text-blue-600"
-            onClick={() => setColor(cambiaColore(4))}
-          ></button>
-        </div>
-=======
     <div
       className={`bg-${color}-600 h-screen w-72 p-4 flex items-center justify-center`}
     >
@@ -93,7 +61,6 @@ const Sidebar: React.FC = () => {
           className="h-10 w-10 rounded-full bg-yellow-600 shadow-xl hover:bg-yellow-700 focus:ring-4 focus:ring-yellow-300 transform transition-transform hover:-translate-y-2 active:translate-y-0 active:shadow-none text-blue-600"
           onClick={() => setColor(cambiaColore(4))}
         ></button>
->>>>>>> b683ab3e3729fea5d31b0e15445b428e94b33f86
       </div>
     </div>
   );
